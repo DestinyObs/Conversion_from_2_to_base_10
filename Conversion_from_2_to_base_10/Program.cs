72f728c6d60cff6bd4c513ef17b9f6afc6c4fa5d
Destiny Obs <105108419+DestinyObs@users.noreply.github.com>
2023-03-28 11:15:33 +0100

    Add project files.

---
+++ Conversion_from_2_to_base_10/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Conversion_from_2_to_base_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a binary number: ");
+            string num = Console.ReadLine();
+
+            int decimalNumber = 0;
+            int power = 1;
+
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+                if (num[i] != '0')
+                {
+                    decimalNumber += power;
+                }
+                power *= 2;
+
+
+            }
+
+            Console.WriteLine($"The decimal equivalent of {num} is {decimalNumber}");
+        }
+    }
+}
